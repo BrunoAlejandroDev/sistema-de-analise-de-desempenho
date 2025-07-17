9fd93de712035c408f92393b16ba9dcc2ed604b7
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-07-17 16:58:11 -0300

    update: adição dos métodos da estrutura pilha e teste da estrutura

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from aluno import Aluno
+from pilha import Pilha
 
-aluno1 = Aluno(536498, 'Bruno')
-aluno1.adicionar_nota(8.5)
-aluno1.adicionar_nota(9.0)
-aluno1.adicionar_falta(2)
+# aluno1 = Aluno(536498, 'Bruno')
+# aluno1.adicionar_nota(8.5)
+# aluno1.adicionar_nota(9.0)
+# aluno1.adicionar_falta(2)
 
-print(aluno1)
+# print(aluno1)
+
+#* ============ TESTE DA PILHA ==============
+pilha_teste = Pilha()
+print(f'Pilha vazia? {pilha_teste.is_empty()}')
+print(f'Tamanho da pilha: {len(pilha_teste)}')
+
+print('\nEmpilhando 10, 20, 30')
+pilha_teste.push(10)
+pilha_teste.push(20)
+pilha_teste.push(30)
+
+print(pilha_teste)
+print(f"Tamanho da pilha: {len(pilha_teste)}")
+print(f"Elemento no topo (peek): {pilha_teste.peek()}")
+
+print('Desempilhar elementos')
+while not pilha_teste.is_empty():
+    elemento_removido = pilha_teste.pop()
+    print(f'removido: {elemento_removido}')
+    
+print(f'Pilha vazia? {pilha_teste.is_empty()}')
--- pilha.py
+++ pilha.py
@@ -19,8 +19,52 @@ class Pilha:
         '''
         Adiciona um elemento ao topo da pilha
         '''
-        novo_no = Node(dado)
+        novo_no = Node(dado) #* cria um novo no
         novo_no.proximo = self.topo
         self.topo = novo_no
         self._tamanho += 1
+    
+    def pop(self):
+        '''
+        Remove um elemento do topo da pilha
+        '''
+        #* Passo 1: verificar se a pilha esta vazia
+        if self.is_empty():
+            raise IndexError('Pilha vazia')
+        
+        #* Passo 2: salvar o elemento que sera removido
+        elemento_removido = self.topo.dado
+        
+        #* Passo 3: stualizar o topo da pilha e o tamanho dela
+        self.topo = self.topo.proximo
+        self._tamanho -= 1
+        
+        #* Passo 4: retorna o elemento removido
+        return elemento_removido 
+    
+    def peek(self):
+        '''
+        Retorna o elemento do topo da pilha mas sem remover o elemento
+        '''
+        #* Passo 1: verificar se a pilha esta vazia
+        if self.is_empty():
+            raise IndexError('Pilha vazia')
+        
+        #* Passo 2: retornar o dado do topo 
+        return self.topo.dado
+    
+    def __len__(self):
+        '''
+        Habilita o uso da função len() na pilha
+        '''
+        return self._tamanho
+    
+    def __str__(self):
+        lista_elementos = []
+        
+        no_atual = self.topo
+        while no_atual:
+            lista_elementos.append(str(no_atual.dado))
+            no_atual = no_atual.proximo
             
+        return ' -> '.join(lista_elementos)
