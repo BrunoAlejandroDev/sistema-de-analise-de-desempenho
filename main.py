8d3437621af0b9345400ae4718ea58a3c9c8ee72
Bruno Alejandro <brunoalejandrodev@gmail.com>
2025-07-31 11:04:25 -0300

    feat: criação do software funcional do sistema

--- main.py
+++ main.py
@@ -1,94 +1,157 @@
-from aluno import Aluno
-from pilha import Pilha
-from fila import Fila
+
+#* Importacoes de arquivo
 from arvore_avl import ArvoreAVL
+from fila import Fila
+from pilha import Pilha
+from aluno import Aluno
 
-# aluno1 = Aluno(10, 'Bruno')
-# aluno1.adicionar_nota(8.5)
-# aluno1.adicionar_nota(9.0)
-# aluno1.adicionar_falta(2)
+#* Menu interativo
+def exibir_menu():
+    print("\n=== Sistema de Análise de Desempenho (SADHE) ===")
+    print("1. Cadastrar Novo Aluno")
+    print("2. Lançar Relatório de Desempenho")
+    print("3. Processar Fila de Relatórios")
+    print("4. Buscar Aluno por Matrícula")
+    print("5. Listar Todos os Alunos (por matrícula)")
+    print("6. Ver Histórico de um Aluno")
+    print("7. Remover Aluno")
+    print("8. Visualizar Árvore")
+    print("9. Sair")
+    return input("Escolha uma opção: ")
 
-# aluno2 = Aluno(20, 'Alicia')
-# aluno2.adicionar_nota(8)
-# aluno2.adicionar_nota(9.4)
-# aluno2.adicionar_falta(1)
+#* Execução do sistema
+def main():
+    #* Variaveis de acesso a estrutura do projeto
+    arvore_alunos = ArvoreAVL()
+    fila_relatorios = Fila()
+    pilhas_historico = {}
 
-# aluno3 = Aluno(30, 'Joaquim')
+    #* Loop de execução do sistema
+    while True:
+        opcao = exibir_menu().strip()
 
-# print(aluno1)
+        #* Opcao 1: cadastrar aluno
+        if opcao == '1':
+            try:
 
-#* ============ TESTE DA PILHA ==============
-# pilha_teste = Pilha()
-# print(f'Pilha vazia? {pilha_teste.is_empty()}')
-# print(f'Tamanho da pilha: {len(pilha_teste)}')
+                matricula = int(input('Digite a matricula: '))
+                nome = input('Digite o nome: ')
 
-# print('\nEmpilhando 10, 20, 30')
-# pilha_teste.push(10)
-# pilha_teste.push(20)
-# pilha_teste.push(30)
+                #* Verificar se aluno já existe no sistema
+                if arvore_alunos.buscar(matricula):
+                    print('Erro. Matricula já cadastrada no sistema.')
+                else:
+                    novo_aluno = Aluno(matricula, nome)
+                    arvore_alunos.insert(matricula, novo_aluno)
+                    pilhas_historico[matricula] = Pilha()
+                    print(f'Aluno {nome} cadastrado com sucesso.')
 
-# print(pilha_teste)
-# print(f"Tamanho da pilha: {len(pilha_teste)}")
-# print(f"Elemento no topo (peek): {pilha_teste.peek()}")
+            except ValueError:
+                print('Erro. A matricula deve ser um número')
 
-# print('Desempilhar elementos')
-# while not pilha_teste.is_empty():
-#     elemento_removido = pilha_teste.pop()
-#     print(f'removido: {elemento_removido}')
-    
-# print(f'Pilha vazia? {pilha_teste.is_empty()}')
+        #* Opcao 2: Lançar relatório
+        elif opcao == '2':
+            try:
+                #* Variaveis
+                matricula = int(input('Digite a matrícula: '))
+                nota = float(input('Digite a nota a ser adicionada: '))
 
-#* ============ TESTE DA FILA ==============
-# fila_teste = Fila()
-# print(f'Fila vazia? {fila_teste.is_empty()}')
-# print(f'Tamanho da Fila: {len(fila_teste)}')
+                #* Verificar se aluno está presente no sistema para adicionar nota
+                if arvore_alunos.buscar(matricula):
+                    relatorio = { #* criar estrutura do relatorio
+                        'matricula' : matricula,
+                        'nota' : nota
+                    }
+                    fila_relatorios.enqueue(relatorio) #* adiciona o relatorio na fila de processamento
+                    print('Relatório adicionado com sucesso à fila de processamento.')
+                else:
+                    print(f'Aluno com matricula {matricula} não identificado no sistema.')
 
-# # adicionar elementos na fila
-# print('\nAdicionando elementos')
-# fila_teste.enqueue(10)
-# fila_teste.enqueue(20)
-# fila_teste.enqueue(30)
+            except ValueError:
+                print('Erro. A matricula deve ser um número')
 
-# print(f'Fila atual: {fila_teste}')
-# print(f'Fila vazia? {fila_teste.is_empty()}')
-# print(f'Tamanho da Fila: {len(fila_teste)}')
+        #* Opcao 3: processar fila de relatórios cadastrados
+        elif opcao == '3':
+            #* Verificar se existem relatorios a serem preenchidos
+            if fila_relatorios.is_empty():
+                print('A fila de relatórios está vazia.')
+            else:
+                relatorio = fila_relatorios.dequeue()
+                aluno = arvore_alunos.buscar(relatorio['matricula']) #* buscar os relatorios de um aluno com base na matricula
 
-# while not fila_teste.is_empty():
-#     elemento_removido = fila_teste.dequeue()
-#     print(f'removido: {elemento_removido}')
-    
-# print(f'Fila vazia? {fila_teste.is_empty()}')
+                if aluno: #* se aluno existir
+                    pilha_relatorios_aluno = pilhas_historico[aluno.matricula]
+                    
+                    estado_antigo = {
+                        'media' : aluno.media_geral
+                    }
+                    pilha_relatorios_aluno.push(estado_antigo)
 
-#* ============ TESTE DOS PERCURSOS DA ARVORE ==============
-# arvore_de_aluno = ArvoreAVL()
-# print("Inserindo alunos na árvore")
-# arvore_de_aluno.insert(aluno1.matricula, aluno1)
-# arvore_de_aluno.insert(aluno2.matricula, aluno2)
-# arvore_de_aluno.insert(aluno3.matricula, aluno3)
-# print("Alunos inseridos com sucesso!")
+                    aluno.adicionar_nota(relatorio['nota'])
+                    print(f'Relatório processado para o aluno {aluno.nome}\nNova média: {aluno.media_geral:.2f}')
 
-# print('Testando os percursos')
-# arvore_de_aluno.in_order()
-# arvore_de_aluno.pre_order()
-# arvore_de_aluno.post_order()
+        #* Opcao 4: procurar aluno
+        elif opcao == '4':
+            try:
+                print('\n=== Processo Atual: Procurar por Aluno no Sistema ===')
+                matricula = int(input('Digite a matricula a ser buscada: '))
+                aluno = arvore_alunos.buscar(matricula)
 
-#* ============ TESTE DO BALANCEAMENTO DA ARVORE ==============
-# arvore_de_aluno.pre_order()
+                if aluno: #* se o aluno tiver sido encontrado
+                    print('-- aluno encontrado!')
+                    print(aluno)
 
-#* ============ TESTE DA REMOÇÃO DA ARVORE ==============
-arvore_teste_remocao = ArvoreAVL()
-arvore_teste_remocao.insert(20, Aluno(20, "Roberto"))
-arvore_teste_remocao.insert(10, Aluno(10, "Ana"))
-arvore_teste_remocao.insert(30, Aluno(30, "Carlos"))
+            except ValueError:
+                print('Erro. A matricula deve ser um número')
 
-arvore_teste_remocao.insert(40, Aluno(40, "Daniel"))
-    
-print("\narvore completa antes da remocao - pre-order")
-arvore_teste_remocao.pre_order()
+        #* Opcao 5: listar alunos
+        elif opcao == '5':
+            print('\n=== Processo Atual: Listar Alunos ===')
+            arvore_alunos.in_order()
 
-#* Testando a remoção de um nó
-print('\nRemocao')
-arvore_teste_remocao.remover(10)
+        #* Opcao 6: ver historico
+        elif opcao == '6':
+            try:
+                print('\n=== Processo Atual: Ver Histórico do Aluno ===')
+                matricula = int(input('- Digite a matrícula para ver o histórico: '))
 
-print('\narvore apos a remocao - pre-order')
-arvore_teste_remocao.pre_order()
+                if matricula in pilhas_historico:
+                    pilha = pilhas_historico[matricula]
+
+                    if pilha.is_empty():
+                        print('-- Nenhuma alteração foi encontrada no histórico do aluno.') 
+                    else:
+                        print(f'-- Histórico do aluno {matricula} (mais novo para o mais antigo)')
+                        no_atual = pilha.topo
+                        while no_atual:
+                            print(no_atual.dado)
+                            no_atual = no_atual.proximo
+                else:
+                    print(f'- Aluno com matricula {matricula} não encontrado')
+            
+            except ValueError:
+                print('- Erro. A matricula deve ser um número')
+
+        #* Opcao 7: remover aluno
+        elif opcao == '7':
+            try:
+                print('\n=== Processo Atual: Remover Aluno ===')
+                matricula = int(input('- Digite a matricula do aluno(a) a ser removido(a): '))
+                arvore_alunos.remover(matricula)
+            except ValueError:
+                print('- Erro. A matricula deve ser um número')
+
+        #* Opcao 8: visualizar arvore
+        elif opcao == '8':
+            arvore_alunos.display()
+
+        #* Opcao 9: sair
+        elif opcao == '9':
+            print('\n=== Saindo do Sistema ===')
+            break
+
+        else:
+            print("Opção inválida. Tente novamente.")
+
+if __name__ == '__main__':
+    main()
